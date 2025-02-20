8d32e322d6cdbacfbdafe5f5b537f87907a47233
Jan-Niklas Wortmann <6104311+niklas-wortmann@users.noreply.github.com>
2025-02-19 21:43:51 -0600

    remove twitter logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FaLinkedin, FaInstagram} from 'react-icons/fa';
-import {FaBluesky, FaXTwitter} from 'react-icons/fa6';
+import {FaBluesky} from 'react-icons/fa6';
 
 const Footer = () => {
   const socialLinks = [
