4ba8ef0d3c5ef6a4ec405ccddbc7f6d1c3030502
Jan-Niklas Wortmann <6104311+niklas-wortmann@users.noreply.github.com>
2025-02-20 12:06:26 -0600

    fix signup form on mobile

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -5,8 +5,8 @@ const NewsletterForm = () => {
 
   return (
       <iframe src="https://embeds.beehiiv.com/7f2a69f2-55ae-49e0-87f9-797d92d34188?slim=true"
-              data-test-id="beehiiv-embed" height="52" width="600" frameBorder="0" scrolling="no"
-              style={{"margin": "auto", borderRadius: "0px !important", backgroundColor: "transparent"}}></iframe>
+              data-test-id="beehiiv-embed" height="52" frameBorder="0" scrolling="no"
+              style={{"margin": "auto", width: '80%', borderRadius: "0px !important", backgroundColor: "transparent"}}></iframe>
   );
 };
 
